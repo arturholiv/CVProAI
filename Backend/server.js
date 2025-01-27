079456709c5da866237ddef7c3f459f0070a2018
arturholiv <artur.henrique45@hotmail.com>
2025-01-27 20:31:15 -0300

    first commit backend

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen((PORT), () => {
+    console.log('Server is running on port ' + PORT);
+});
+    
