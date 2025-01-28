5e329fb38527dd950446557a69f55aeb6a266bee
arturholiv <artur.henrique45@hotmail.com>
2025-01-27 21:52:47 -0300

    feat: deepseek route implementation

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
+const main = require('./deepseek.js');
 const express = require('express');
 const dotenv = require('dotenv');
 
 const app = express();
+
+app.use(express.json());
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
@@ -9,6 +12,14 @@ const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/api/deepseek', (req, res) => {
+  const {input} = req.body;
+  console.log(input);
+  main(input).then((response) => {
+    res.send(response);
+  });
 });
 
 app.listen((PORT), () => {
