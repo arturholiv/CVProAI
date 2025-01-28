ef5a89ab70ed7c114189cd65e10f8bc63ab035e8
arturholiv <artur.henrique45@hotmail.com>
2025-01-27 21:52:28 -0300

    Feat: updates deepseek.js

--- Backend/deepseek.js
+++ Backend/deepseek.js
@@ -1,5 +1,6 @@
 const OpenAI = require('openai');
 const dotenv = require('dotenv');
+dotenv.config();
 
 const openai = new OpenAI({
     baseURL: 'https://api.deepseek.com',
@@ -8,14 +9,19 @@ const openai = new OpenAI({
 
 const fields = "name, email, phone, address, links, summary, experience, education, skills, projects, certifications, languages, references.";
 
-export async function main() {
+async function main(input) {
     const completion = await openai.chat.completions.create({
       messages: [{ role: "system", content: "You are a resume builder. You will send the resume data based in the input i will send to you as a json with the following keys/fields:" +
         fields + " links being an array of strings. experience and education must have a start and end date. The rest are strings." 
-       }],
+       },
+      {
+        role: "user",
+        content: input
+      }],
       model: "deepseek-chat",
     });
   
-    console.log(completion.choices[0].message.content);
+    return completion.choices[0].message.content;
   }
   
+  module.exports = main;
